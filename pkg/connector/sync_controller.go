20966b74d0e1366c2d9532f31b28050a1a50c500
Ludvig Rhodin <lrhodin@gmail.com>
2026-02-13 11:08:43 -0800

    remove debug logging for skipped messages

--- pkg/connector/sync_controller.go
+++ pkg/connector/sync_controller.go
@@ -284,12 +284,6 @@ func (c *IMClient) ingestCloudMessages(
 			}
 		}
 		if portalID == "" {
-			zerolog.Ctx(ctx).Debug().
-				Str("guid", msg.Guid).
-				Str("cloud_chat_id", msg.CloudChatId).
-				Str("sender", msg.Sender).
-				Bool("is_from_me", msg.IsFromMe).
-				Msg("Skipping cloud message: no portal ID resolved")
 			counts.Skipped++
 			continue
 		}
