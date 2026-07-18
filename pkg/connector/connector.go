8d5cf801298f32323f3a781e2e9ef3b3f948e5b9
jasonlaguidice <19523621+jasonlaguidice@users.noreply.github.com>
2026-07-17 22:39:53 -0700

    Replace network icon w/ Rustpush/OpenBubbles icon

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -39,7 +39,7 @@ func (c *IMConnector) GetName() bridgev2.BridgeName {
 	return bridgev2.BridgeName{
 		DisplayName:          "iMessage",
 		NetworkURL:           "https://support.apple.com/messages",
-		NetworkIcon:          "mxc://maunium.net/tManJEpANASZvDVzvRvhILdl",
+		NetworkIcon:          "mxc://shadowdrake.org/zUZxFBMoCbfmELVGwjeVyins",
 		NetworkID:            "imessage",
 		BeeperBridgeType:     "imessagego",
 		DefaultPort:          29332,
